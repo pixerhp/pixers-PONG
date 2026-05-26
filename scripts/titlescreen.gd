e45c62c9fe114245c76710b84b1ad79cb01e7955
Pixer H. P. <99830732+pixerhp@users.noreply.github.com>
2026-05-26 19:10:32 -0400

    Start work on settings menu

--- scripts/titlescreen.gd
+++ scripts/titlescreen.gd
@@ -15,14 +15,11 @@ func reset_framing(content_size: Vector2i):
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/topscenes/gameplay_topscene.tscn")
 
-
 func _on_settings_button_pressed():
-	pass # Replace with function body.
-
+	%SettingsMenu.visible = true
 
 func _on_extras_button_pressed():
 	pass # Replace with function body.
-
 
 func _on_quit_button_pressed():
 	get_tree().quit()
